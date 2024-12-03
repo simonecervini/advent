bd9d20b0f63da9fca866d7be23c1e5968dac3af9
Simone Cervini <sc.cervini@gmail.com>
2024-12-03 21:50:11 +0100

    2023-12-20 - Advent of TypeScript (not completed in 2023)

---
+++ src/advent-of-typescript-2023/20.ts
@@ -0,0 +1,124 @@
+type Letters = {
+  A: ["█▀█ ", "█▀█ ", "▀ ▀ "];
+  B: ["█▀▄ ", "█▀▄ ", "▀▀  "];
+  C: ["█▀▀ ", "█ ░░", "▀▀▀ "];
+  E: ["█▀▀ ", "█▀▀ ", "▀▀▀ "];
+  H: ["█ █ ", "█▀█ ", "▀ ▀ "];
+  I: ["█ ", "█ ", "▀ "];
+  M: ["█▄░▄█ ", "█ ▀ █ ", "▀ ░░▀ "];
+  N: ["█▄░█ ", "█ ▀█ ", "▀ ░▀ "];
+  P: ["█▀█ ", "█▀▀ ", "▀ ░░"];
+  R: ["█▀█ ", "██▀ ", "▀ ▀ "];
+  S: ["█▀▀ ", "▀▀█ ", "▀▀▀ "];
+  T: ["▀█▀ ", "░█ ░", "░▀ ░"];
+  Y: ["█ █ ", "▀█▀ ", "░▀ ░"];
+  W: ["█ ░░█ ", "█▄▀▄█ ", "▀ ░ ▀ "];
+  " ": ["░", "░", "░"];
+  ":": ["#", "░", "#"];
+  "*": ["░", "#", "░"];
+};
+
+type Split<
+  Str,
+  Separator extends string = "\n",
+  Arr extends string[] = [],
+> = Str extends `${infer A}${Separator}${infer B}`
+  ? Split<B, Separator, [...Arr, A]>
+  : [...Arr, Str];
+type test_Split = Split<"aaa\nbbb\nccc">; // =>
+
+type Flatten<Arr extends any[], Out extends any[] = []> = Arr extends [
+  infer U,
+  ...infer Rest,
+]
+  ? U extends Array<any>
+    ? Flatten<[...Flatten<U>, ...Rest], Out>
+    : Flatten<Rest, [...Out, U]>
+  : Out;
+type test_Flatten = Flatten<[["a1", "a2", "a3"], ["b1", "b2", "b3"]]>; // =>
+
+type ToLetters<
+  Str extends string,
+  Out extends [string, string, string] = ["", "", ""],
+> = Str extends `${infer Char extends keyof Letters}${infer Rest}`
+  ? ToLetters<
+      Rest,
+      [
+        `${Out[0]}${Letters[Char][0]}`,
+        `${Out[1]}${Letters[Char][1]}`,
+        `${Out[2]}${Letters[Char][2]}`,
+      ]
+    >
+  : Out;
+type test_ToLetters = ToLetters<"CHAT">; // =>
+
+// A variant of Split<> that uses ToLetters<>
+type SplitAndToLetters<
+  Str extends string,
+  Separator extends string = "\n",
+  Arr extends unknown[] = [],
+> = Str extends `${infer A}${Separator}${infer B}`
+  ? SplitAndToLetters<B, Separator, [...Arr, ToLetters<A>]>
+  : [...Arr, ToLetters<Str>];
+
+type ToAsciiArt<T extends string> = Flatten<SplitAndToLetters<Uppercase<T>>>;
+
+// -- Tests --
+
+import { Equal, Expect } from "type-testing";
+
+type test_0_actual = ToAsciiArt<"   * : * Merry * : *   \n  Christmas  ">;
+//   ^?
+type test_0_expected = [
+  "░░░░░#░░░█▄░▄█ █▀▀ █▀█ █▀█ █ █ ░░░#░░░░░",
+  "░░░#░░░#░█ ▀ █ █▀▀ ██▀ ██▀ ▀█▀ ░#░░░#░░░",
+  "░░░░░#░░░▀ ░░▀ ▀▀▀ ▀ ▀ ▀ ▀ ░▀ ░░░░#░░░░░",
+  "░░█▀▀ █ █ █▀█ █ █▀▀ ▀█▀ █▄░▄█ █▀█ █▀▀ ░░",
+  "░░█ ░░█▀█ ██▀ █ ▀▀█ ░█ ░█ ▀ █ █▀█ ▀▀█ ░░",
+  "░░▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀▀▀ ░▀ ░▀ ░░▀ ▀ ▀ ▀▀▀ ░░",
+];
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
+
+type test_1_actual = ToAsciiArt<"  Happy new  \n  * : * : * Year * : * : *  ">;
+//   ^?
+type test_1_expected = [
+  "░░█ █ █▀█ █▀█ █▀█ █ █ ░█▄░█ █▀▀ █ ░░█ ░░",
+  "░░█▀█ █▀█ █▀▀ █▀▀ ▀█▀ ░█ ▀█ █▀▀ █▄▀▄█ ░░",
+  "░░▀ ▀ ▀ ▀ ▀ ░░▀ ░░░▀ ░░▀ ░▀ ▀▀▀ ▀ ░ ▀ ░░",
+  "░░░░#░░░#░░░█ █ █▀▀ █▀█ █▀█ ░░░#░░░#░░░░",
+  "░░#░░░#░░░#░▀█▀ █▀▀ █▀█ ██▀ ░#░░░#░░░#░░",
+  "░░░░#░░░#░░░░▀ ░▀▀▀ ▀ ▀ ▀ ▀ ░░░#░░░#░░░░",
+];
+type test_1 = Expect<Equal<test_1_actual, test_1_expected>>;
+
+type test_2_actual =
+  ToAsciiArt<"  * : * : * : * : * : * \n  Trash  \n  * : * : * : * : * : * ">;
+//   ^?
+type test_2_expected = [
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░▀█▀ █▀█ █▀█ █▀▀ █ █ ░░",
+  "░░░█ ░██▀ █▀█ ▀▀█ █▀█ ░░",
+  "░░░▀ ░▀ ▀ ▀ ▀ ▀▀▀ ▀ ▀ ░░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+];
+type test_2 = Expect<Equal<test_2_actual, test_2_expected>>;
+
+type test_3_actual =
+  ToAsciiArt<"  : * : * : * : * : * : * : \n  Ecyrbe  \n  : * : * : * : * : * : * : ">;
+//   ^?
+type test_3_expected = [
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░█▀▀ █▀▀ █ █ █▀█ █▀▄ █▀▀ ░░",
+  "░░█▀▀ █ ░░▀█▀ ██▀ █▀▄ █▀▀ ░░",
+  "░░▀▀▀ ▀▀▀ ░▀ ░▀ ▀ ▀▀  ▀▀▀ ░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+];
+type test_3 = Expect<Equal<test_3_actual, test_3_expected>>;
