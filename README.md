078d0e54565b1268ecb7ef4f91f2a470f0272c10
Simone Cervini <sc.cervini@gmail.com>
2024-12-11 00:29:34 +0100

    Add readme and cli

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
-# Getting Started
+# Advent 🎄✨
 
-1. Edit `index.ts` to change the code.
-2. Execute the following command to run the code.
+This repository contains my solutions for **Advent of Code** and **Advent of TypeScript**, all written in TypeScript. 🎅🧑‍🎄 Use the CLI included in the repo to run and test the solutions.
 
+## Installation 🎁
+
+Make sure you have **pnpm** installed, then clone this repository and navigate to its directory.
+
+```bash
+pnpm install
 ```
-pnpm play
+
+## Usage 🛷
+
+Run the CLI using the `dev` command and specify one of the subcommands:
+
+### Advent of Code ❄️
+
+```bash
+pnpm dev aoc -y <year> <file>
 ```
+
+- `-y, --year`: Specify the year (defaults to the current year).
+- `<file>`: The challenge file number to execute (e.g., `1`, `03`).
+
+Example:
+
+```bash
+pnpm dev aoc -y 2023 01
+```
+
+### Advent of TypeScript 🌟
+
+```bash
+pnpm dev aot -y <year> <file>
+```
+
+- `-y, --year`: Specify the year (defaults to the current year).
+- `<file>`: The TypeScript file number to check (e.g., `2`, `04`).
+
+Example:
+
+```bash
+pnpm dev aot -y 2023 02
+```
+
+## License 🎄
+
+This project is licensed under the MIT License. Feel free to share, modify, and enjoy! ✨
---
+++ dev.ts
@@ -0,0 +1,66 @@
+import { Command } from "commander";
+
+const program = new Command();
+
+const getCurrentYear = () => new Date().getFullYear().toString();
+
+program.name("advent").version("12.25.0");
+
+program
+  .command("aoc")
+  .alias("advent-of-code")
+  .description("Run a specific Advent of Code file for the given year")
+  .option(
+    "-y, --year <year>",
+    "Specify the year (default: current year)",
+    getCurrentYear()
+  )
+  .argument("<file>", "Specify the file number to execute (e.g., 1, 2, 03, 04)")
+  .action((file, options) => {
+    const year = options.year || getCurrentYear();
+
+    Bun.spawnSync(
+      [
+        "pnpm",
+        "bun",
+        "--watch",
+        "run",
+        `${year}/advent-of-code/${String(file).padStart(2, "0")}.ts`,
+      ],
+      {
+        stdout: "inherit",
+      }
+    );
+  });
+
+program
+  .command("aot")
+  .alias("advent-of-typescript")
+  .description(
+    "Run TypeScript type checks for a specific Advent of TypeScript file."
+  )
+  .option(
+    "-y, --year <year>",
+    "Specify the year (default: current year)",
+    getCurrentYear()
+  )
+  .argument("<file>", "Specify the file number to check (e.g., 1, 2, 03, 04)")
+  .action((file, options) => {
+    const year = options.year || getCurrentYear();
+
+    Bun.spawnSync(
+      [
+        "pnpm",
+        "tsc",
+        "--noEmit",
+        "--watch",
+        "--skipLibCheck",
+        `${year}/advent-of-typescript/${String(file).padStart(2, "0")}.ts`,
+      ],
+      {
+        stdout: "inherit",
+      }
+    );
+  });
+
+program.parse(process.argv);
